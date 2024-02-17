2faeba02bfe455a977e8c04ce2eb4a1e2ddce108
julio cesar <juliodantas19@gmail.com>
2024-02-16 23:08:59 -0300

    Commit inicial do projeto do E-commerece. Estrutura do projeto Maven.

---
+++ src/main/java/br/com/everin/everinproject/EverinProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.everin.everinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverinProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/everin/everinproject/EverinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.everin.everinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
